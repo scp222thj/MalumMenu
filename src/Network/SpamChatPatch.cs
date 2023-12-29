f93fc3604e72c546b396ebca1aee2fe50d473eb7
scp222thj <82206202+scp222thj@users.noreply.github.com>
2023-12-29 18:22:56 +0100

    Improved SpamChat cheat

--- src/Network/SpamChatPatch.cs
+++ src/Network/SpamChatPatch.cs
@@ -1,44 +1,71 @@
 using HarmonyLib;
 using Hazel;
+using UnityEngine;
 using System.Text.RegularExpressions;
 
 namespace MalumMenu;
 
 [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSendChat))]
-public static class Network_SpamChatPostfix
+public static class Network_SpamTextPostfix
 {
-    //Prefix patch of PlayerControl.RpcSendChat
+    public static string spamText;
+    private static float lastChatTime = 0f;
+    private static float chatDelay = 0.5f; // Delay between messages in seconds
+
+    // Prefix patch of PlayerControl.RpcSendChat
     public static bool Prefix(string chatText, PlayerControl __instance)
     {
-        if (CheatSettings.spamChat){ //Run custom code if the spamChat cheat is active
+        if (CheatSettings.spamChat)
+        {
             chatText = Regex.Replace(chatText, "<.*?>", string.Empty);
-
             if (string.IsNullOrWhiteSpace(chatText))
             {
                 return false;
             }
 
-            var HostData = AmongUsClient.Instance.GetHost();
-            if (HostData != null && !HostData.Character.Data.Disconnected){
-
-                //Sends a forged SendChat RPC call from each player to all players
-                //This generates an identical fake chat message coming from each player, spamming the chat
-                foreach (var sender in PlayerControl.AllPlayerControls)
-                {
-                    foreach (var recipient in PlayerControl.AllPlayerControls)
-                    {
-                        MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(sender.NetId, (byte)RpcCalls.SendChat, SendOption.None, AmongUsClient.Instance.GetClientIdFromCharacter(recipient.Data.Object));
-                        writer.Write(chatText);
-                        AmongUsClient.Instance.FinishRpcImmediately(writer);
-                    }
-                }
-                
-            }
-
-            return false; //Skip the original method when the cheat is active
-
+            spamText = chatText;
+            return false; // Skip the original method when the cheat is active
         }
 
-        return true; //Send a normal chat message if the cheat is not active
+        return true; // Send a normal chat message if the cheat is not active
+    }
+
+    public static void Update()
+    {
+        if (CheatSettings.spamChat && spamText != null && Time.time - lastChatTime >= chatDelay)
+        {
+            lastChatTime = Time.time;
+            SendSpamChat();
+        }
+    }
+
+    private static void SendSpamChat()
+    {
+        var HostData = AmongUsClient.Instance.GetHost();
+        if (HostData != null && !HostData.Character.Data.Disconnected)
+        {
+            foreach (var sender in PlayerControl.AllPlayerControls)
+            {
+                foreach (var recipient in PlayerControl.AllPlayerControls)
+                {
+                    MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(sender.NetId, (byte)RpcCalls.SendChat, SendOption.None, AmongUsClient.Instance.GetClientIdFromCharacter(recipient.Data.Object));
+                    writer.Write(spamText);
+                    AmongUsClient.Instance.FinishRpcImmediately(writer);
+                }
+            }
+        }
+    }
+}
+
+[HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.LateUpdate))]
+public static class Network_SpamChatPostfix
+{
+    public static void Postfix(PlayerPhysics __instance)
+    {
+        Network_SpamTextPostfix.Update();
+        if (!CheatSettings.spamChat)
+        {
+            Network_SpamTextPostfix.spamText = null;
+        }
     }
 }
