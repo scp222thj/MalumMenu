a8ba49a15c21f3db3f3510e99af43dfe64481899
scp222thj <82206202+scp222thj@users.noreply.github.com>
2024-01-05 16:27:30 +0100

    Small bugfixes

--- src/Spoofing/SpoofingCheatsPatch.cs
+++ src/Spoofing/SpoofingCheatsPatch.cs
@@ -23,30 +23,32 @@ public static class SpoofFriendCode_EOSManager_Update_Postfix
     //Postfix patch of EOSManager.Update to spoof friend codes
     public static void Postfix(EOSManager __instance)
     {
-        if (CheatToggles.spoofRandomFC){ //spoofRandomFC cheat logic
-            if (defaultFC == null){
-                defaultFC = __instance.FriendCode; //Save default friend code before randomizing it
+        try{
+            if (CheatToggles.spoofRandomFC){ //spoofRandomFC cheat logic
+                if (defaultFC == null){
+                    defaultFC = __instance.FriendCode; //Save default friend code before randomizing it
+                }
+
+                //Generate and save a completly random friend code
+                string username = DestroyableSingleton<AccountManager>.Instance.GetRandomName().ToLower();
+                string discriminator = new System.Random().Next(1000, 10000).ToString();
+                __instance.FriendCode = username + "#" + discriminator;
+
+                return; //Do not use friend code spoofing from config if spoofRandomFC cheat is already going on
             }
 
-            //Generate and save a completly random friend code
-            string username = DestroyableSingleton<AccountManager>.Instance.GetRandomName().ToLower();
-            string discriminator = new System.Random().Next(1000, 10000).ToString();
-            __instance.FriendCode = username + "#" + discriminator;
+            if (MalumMenu.spoofFriendCode.Value != "" && MalumMenu.spoofFriendCode.Value != __instance.FriendCode){ //friendCodeSpoofing from config cheat logic
+                __instance.FriendCode = MalumMenu.spoofFriendCode.Value; //Set custom friend code from config file
+                
+                return;
+            }
 
-            return; //Do not use friend code spoofing from config if spoofRandomFC cheat is already going on
-        }
-
-        if (MalumMenu.spoofFriendCode.Value != "" && MalumMenu.spoofFriendCode.Value != __instance.FriendCode){ //friendCodeSpoofing from config cheat logic
-            __instance.FriendCode = MalumMenu.spoofFriendCode.Value; //Set custom friend code from config file
-            
-            return;
-        }
-
-        //Return to default friend code if both cheats are disabled
-        if (defaultFC != null){
-            __instance.FriendCode = defaultFC;
-            defaultFC = null;
-        }
+            //Return to default friend code if both cheats are disabled
+            if (defaultFC != null){
+                __instance.FriendCode = defaultFC;
+                defaultFC = null;
+            }
+        }catch{}
     }
 }
 
