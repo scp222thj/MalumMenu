aa441bbe45bd7bc0cdb3fa85df4230eac6ea415f
thonk <github.com@villainsrule.xyz>
2024-01-21 13:04:03 -0500

    shapeshit player to player

--- src/Trolling/Shapeshift/ShapeshiftCheatPatch.cs
+++ src/Trolling/Shapeshift/ShapeshiftCheatPatch.cs
@@ -11,51 +11,54 @@ public static class ShapeshiftCheat_PlayerPhysics_LateUpdate_Postfix
 {
     //Postfix patch of PlayerPhysics.LateUpdate to open player pick menu to shapeshift into any player
     public static bool isActive;
-    public static void Postfix(PlayerPhysics __instance){
-        if (CheatToggles.shapeshiftCheat){
-
-            if (!isActive){
-
+    public static PlayerControl shiftingPlayer = null;
+    public static PlayerControl shiftingTarget = null;
+    public static void Postfix(PlayerPhysics __instance)
+    {
+        if (CheatToggles.shapeshiftCheat)
+        {
+            if (!isActive)
+            {
                 //Close any player pick menus already open & their cheats
-                if (Utils_PlayerPickMenu.playerpickMenu != null){
+                if (Utils_PlayerPickMenu.playerpickMenu != null)
+                {
                     Utils_PlayerPickMenu.playerpickMenu.Close();
                     CheatToggles.DisablePPMCheats("shapeshiftCheat");
                 }
 
                 List<GameData.PlayerInfo> playerDataList = new List<GameData.PlayerInfo>();
 
-                //All players are saved to playerList apart from LocalPlayer
-                foreach (var player in PlayerControl.AllPlayerControls){
-                    if (!player.AmOwner){
-                        playerDataList.Add(player.Data);
-                    }
+                // All players are saved to playerList, including the localplayer
+                foreach (var player in PlayerControl.AllPlayerControls)
+                {
+                    playerDataList.Add(player.Data);
                 }
 
-                //New player pick menu made for shapeshifting into any player
-                Utils_PlayerPickMenu.openPlayerPickMenu(playerDataList, (Action) (() =>
+                // Two consecutive player pick menus, first for the shifter, second for the target of the shifter
+                Utils_PlayerPickMenu.openPlayerPickMenu(playerDataList, (Action)(() =>
                 {
+                    shiftingPlayer = Utils_PlayerPickMenu.targetPlayerData.Object;
+                    Utils_PlayerPickMenu.openPlayerPickMenu(playerDataList, (Action)(() =>
+                    {
+                        shiftingTarget = Utils_PlayerPickMenu.targetPlayerData.Object;
 
-                    var HostData = AmongUsClient.Instance.GetHost();
-                    if (HostData != null && !HostData.Character.Data.Disconnected){
-
-                        Utils.ShapeshiftPlayer(PlayerControl.LocalPlayer, Utils_PlayerPickMenu.targetPlayerData.Object, !CheatToggles.noShapeshiftAnim); //Compatible with noShapeshiftAnim
-
-                    }
-                
+                        var HostData = AmongUsClient.Instance.GetHost();
+                        if (HostData != null && !HostData.Character.Data.Disconnected)
+                        {
+                            Utils.ShapeshiftPlayer(shiftingPlayer, Utils_PlayerPickMenu.targetPlayerData.Object, !CheatToggles.noShapeshiftAnim); // Compatible with noShapeshiftAnim
+                            shiftingPlayer = null;
+                            shiftingTarget = null;
+                            CheatToggles.shapeshiftCheat = false;
+                        }
+                    }));
                 }));
 
                 isActive = true;
             }
-
-            //Deactivate cheat if menu is closed
-            if (Utils_PlayerPickMenu.playerpickMenu == null){
-                CheatToggles.shapeshiftCheat = false;
-            }
-
-        }else{
-            if (isActive){
-                isActive = false;
-            }
+        }
+        else if (isActive)
+        {
+            isActive = false;
         }
     }
 }
