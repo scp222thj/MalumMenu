f3095a428016330dd3903a5fac0d0c4e1baa296e
Pietrodjaowjao <nicoleobenetti@gmail.com>
2024-03-09 14:38:14 -0300

    ensure friend code changes

--- src/Patches/EOSManagerPatches.cs
+++ src/Patches/EOSManagerPatches.cs
@@ -22,8 +22,8 @@ public static class EOSManager_SetSpoofFC
     public static void Postfix(EOSManager __instance)
     {
         if (!MalumMenu.incognitoMode.Value) return;
-        __instance.FriendCode = MalumMenu.spoofFriendCode.Value;
         DataManager.Player.Account.LoginStatus = EOSManager.AccountLoginStatus.LoggedIn;
+        __instance.FriendCode = MalumMenu.spoofFriendCode.Value;
     }
 }
 
