0a491f064ce84bad2d91865674ff1e2d6fb8a889
scp222thj <82206202+scp222thj@users.noreply.github.com>
2024-03-19 21:04:00 +0100

    Update MenuUI.cs

--- src/UI/MenuUI.cs
+++ src/UI/MenuUI.cs
@@ -4,29 +4,8 @@ using UnityEngine;
 namespace MalumMenu;
 public class MenuUI
 {
-    public static bool _isMyUIOpen = true;
     public static void render()
     {
-        if (_isMyUIOpen)
-        {
-            var dummy2 = true;
-            if (ImGui.Begin("Test Window", ref dummy2, (int)ImGuiWindowFlags.None))
-            {
-                ImGui.Text("Hello, World!");
-
-
-                if (ImGui.Button("Click me"))
-                {
-                    // Interacting with the unity api must be done from the unity main thread
-                    // Can just use the dispatcher shipped with the library for that
-                    UnityMainThreadDispatcher.Enqueue(() =>
-                    {
-                        Debug.Log("test");
-                    });
-                }
-            }
-
-            ImGui.End();
-        }
+        ImGui.ShowDemoWindow();
     }
 }
