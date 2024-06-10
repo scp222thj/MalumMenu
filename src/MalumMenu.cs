4b678b900da362c6a63d68de981a167b317789a7
scp222thj <82206202+scp222thj@users.noreply.github.com>
2024-06-10 20:08:26 +0200

    Update the code to latest AU version

--- src/MalumMenu.cs
+++ src/MalumMenu.cs
@@ -16,7 +16,7 @@ public partial class MalumMenu : BasePlugin
 {
     public Harmony Harmony { get; } = new(Id);
     public static string malumVersion = "2.3.0";
-    public static List<string> supportedAU = new List<string> { "2023.11.28", "2024.3.5" };
+    public static List<string> supportedAU = new List<string> { "2023.11.28", "2024.3.5", "2024.6.4" };
     public static MenuUI menuUI;
     // public static ConsoleUI consoleUI;
     public static ConfigEntry<string> menuKeybind;
