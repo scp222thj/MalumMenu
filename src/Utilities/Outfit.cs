dbdff8fbfa793365cf7eb4616146aa010a355de1
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-02-18 20:45:15 +0100

    Refactor naming convention for Outfit class

--- src/Utilities/Outfit.cs
+++ src/Utilities/Outfit.cs
@@ -1,67 +1,67 @@
 namespace MalumMenu;
 
-public static class Outfits
+public static class Outfit
 {
-    public static NetworkedPlayerInfo.PlayerOutfit shapeshifter = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Shapeshifter = new()
     {
         ColorId = 0,
         SkinId = "skin_screamghostface",
         VisorId = "visor_eliksni"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit phantom = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Phantom = new()
     {
         ColorId = 0,
         HatId = "hat_screamghostface",
         SkinId = "skin_screamghostface"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit viper = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Viper = new()
     {
         ColorId = 0,
         SkinId = "skin_Hazmat-Greenskin",
         VisorId = "visor_animesunglassesVisor"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit impostor = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Impostor = new()
     {
         ColorId = 0
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit tracker = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Tracker = new()
     {
         ColorId = 10,
         SkinId = "skin_rhm"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit noisemaker = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Noisemaker = new()
     {
         ColorId = 10,
         HatId = "hat_pk03_Headphones"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit engineer = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Engineer = new()
     {
         ColorId = 10,
         SkinId = "skin_Mech",
         VisorId = "visor_D2CGoggles"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit scientist = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Scientist = new()
     {
         ColorId = 10,
         SkinId = "skin_Science",
         VisorId = "visor_pk01_PaperMaskVisor"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit detective = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Detective = new()
     {
         ColorId = 10,
         HatId = "hat_pk05_Fedora",
         SkinId = "skin_SuitW"
     };
 
-    public static NetworkedPlayerInfo.PlayerOutfit crewmate = new()
+    public static NetworkedPlayerInfo.PlayerOutfit Crewmate = new()
     {
         ColorId = 10
     };
