35600659ee1df936ec0106aa187f30f5889f5937
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-02-21 16:10:52 +0100

    Refactor CosmeticsUnlocker for reintegration

--- src/Cheats/CosmeticsUnlocker.cs
+++ src/Cheats/CosmeticsUnlocker.cs
@@ -4,43 +4,54 @@ public static class CosmeticsUnlocker
     public static void unlockCosmetics(HatManager hatManager)
     {
         if (!CheatToggles.freeCosmetics) return;
-        foreach(var bundle in hatManager.allBundles){ //Bundles
+
+        foreach(var bundle in hatManager.allBundles) // Bundles
+        {
             bundle.Free = true;
         }
 
-        foreach(var featuredBundle in hatManager.allFeaturedBundles){ //Featured Bundles
+        foreach(var featuredBundle in hatManager.allFeaturedBundles) // Featured Bundles
+        {
             featuredBundle.Free = true;
         }
 
-        foreach(var featuredCube in hatManager.allFeaturedCubes){ //Featured Cosmicubes
+        foreach(var featuredCube in hatManager.allFeaturedCubes) // Featured Cosmicubes
+        {
             featuredCube.Free = true;
         }
 
-        foreach(var featuredItem in hatManager.allFeaturedItems){ //Featured Items
+        foreach(var featuredItem in hatManager.allFeaturedItems) // Featured Items
+        {
             featuredItem.Free = true;
         }
 
-        foreach(var hat in hatManager.allHats){ //Hats
+        foreach(var hat in hatManager.allHats) // Hats
+        {
             hat.Free = true;
         }
 
-        foreach(var nameplate in hatManager.allNamePlates){ //NamePlates
+        foreach(var nameplate in hatManager.allNamePlates) // NamePlates
+        {
             nameplate.Free = true;
         }
 
-        foreach(var pet in hatManager.allPets){ //Pets
+        foreach(var pet in hatManager.allPets) // Pets
+        {
             pet.Free = true;
         }
 
-        foreach(var skin in hatManager.allSkins){ //Skins
+        foreach(var skin in hatManager.allSkins) // Skins
+        {
             skin.Free = true;
         }
 
-        foreach(var starBundle in hatManager.allStarBundles){ //Star Bundles
+        foreach(var starBundle in hatManager.allStarBundles) // Star Bundles
+        {
             starBundle.price = 0; // StarBundles don't have a Free property, so price is changed instead
         }
 
-        foreach(var visor in hatManager.allVisors){ //Visors
+        foreach(var visor in hatManager.allVisors) // Visors
+        {
             visor.Free = true;
         }
     }
