3990a8b40879f29c79104402f694e668f3db97cf
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-02-21 20:11:42 +0100

    Refactor LogicGameFlowPatches for reintegration

--- src/Patches/LogicGameFlowPatches.cs
+++ src/Patches/LogicGameFlowPatches.cs
@@ -5,10 +5,7 @@ namespace MalumMenu;
 [HarmonyPatch(typeof(LogicGameFlowNormal), nameof(LogicGameFlowNormal.CheckEndCriteria))]
 public static class LogicGameFlowNormal_CheckEndCriteria
 {
-    /// <summary>
-    /// Prefix patch of LogicGameFlowNormal.CheckEndCriteria to prevent the game from ending
-    /// </summary>
-    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    // Prefix patch of LogicGameFlowNormal.CheckEndCriteria to prevent a running game from ending
     public static bool Prefix()
     {
         return !CheatToggles.noGameEnd;
@@ -18,10 +15,8 @@ public static class LogicGameFlowNormal_CheckEndCriteria
 [HarmonyPatch(typeof(LogicGameFlowNormal), nameof(LogicGameFlowNormal.IsGameOverDueToDeath))]
 public static class LogicGameFlowNormal_IsGameOverDueToDeath
 {
-    /// <summary>
-    /// Postfix patch of LogicGameFlowNormal.IsGameOverDueToDeath to prevent the game from ending
-    /// </summary>
-    /// <param name="__result">Original return value of <c>IsGameOverDueToDeath</c>.</param>
+    // Postfix patch of LogicGameFlowNormal.IsGameOverDueToDeath to ensure the game does not stall
+    // after an exile that should have triggered game over
     public static void Postfix(ref bool __result)
     {
         if (CheatToggles.noGameEnd){
@@ -34,10 +29,7 @@ public static class LogicGameFlowNormal_IsGameOverDueToDeath
 [HarmonyPatch(typeof(LogicGameFlowHnS), nameof(LogicGameFlowHnS.CheckEndCriteria))]
 public static class LogicGameFlowHnS_CheckEndCriteria
 {
-    /// <summary>
-    /// Prefix patch of LogicGameFlowHnS.CheckEndCriteria to prevent the game from ending
-    /// </summary>
-    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    // Prefix patch of LogicGameFlowHnS.CheckEndCriteria to prevent a running HnS game from ending
     public static bool Prefix()
     {
         return !CheatToggles.noGameEnd;
@@ -47,10 +39,8 @@ public static class LogicGameFlowHnS_CheckEndCriteria
 [HarmonyPatch(typeof(LogicGameFlowHnS), nameof(LogicGameFlowHnS.IsGameOverDueToDeath))]
 public static class LogicGameFlowHnS_IsGameOverDueToDeath
 {
-    /// <summary>
-    /// Postfix patch of LogicGameFlowHnS.IsGameOverDueToDeath to prevent the game from ending
-    /// </summary>
-    /// <param name="__result">Original return value of <c>IsGameOverDueToDeath</c>.</param>
+    // Postfix patch of LogicGameFlowNormal.IsGameOverDueToDeath to ensure the HnS game does not stall
+    // after an exile that should have triggered game over
     public static void Postfix(ref bool __result)
     {
         if (CheatToggles.noGameEnd){
