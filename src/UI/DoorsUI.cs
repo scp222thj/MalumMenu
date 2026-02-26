121e65cacae2beddc5ef04fb9e1160483bbde0ff
Frankmon <83294327+Baumdc@users.noreply.github.com>
2026-02-26 16:33:46 +0100

    Simplify color handling in DoorsUI
    Refactored color application logic in OnGUI method.

--- src/UI/DoorsUI.cs
+++ src/UI/DoorsUI.cs
@@ -13,23 +13,7 @@ public class DoorsUI : MonoBehaviour
     {
         if (!CheatToggles.showDoorsMenu || !MenuUI.isGUIActive || MenuUI.isPanicked) return;
 
-        if (CheatToggles.rgbMode)
-        {
-            GUI.backgroundColor = Color.HSVToRGB(MenuUI.hue, 1f, 1f);
-        }
-        else
-        {
-            var configHtmlColor = MalumMenu.menuHtmlColor.Value;
-
-            if (ColorUtility.TryParseHtmlString(configHtmlColor, out var uiColor))
-            {
-                GUI.backgroundColor = uiColor;
-            }
-            else if (!configHtmlColor.StartsWith("#") && ColorUtility.TryParseHtmlString("#" + configHtmlColor, out uiColor))
-            {
-                GUI.backgroundColor = uiColor;
-            }
-        }
+        MenuHelper.ApplyMenuColor();
 
         _windowRect = GUI.Window(2, _windowRect, (GUI.WindowFunction)DoorsWindow, "Doors");
     }
