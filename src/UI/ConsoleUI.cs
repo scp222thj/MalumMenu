8c0b559979fc43acdc66e7f784cba7ccde988105
Frankmon <83294327+Baumdc@users.noreply.github.com>
2026-02-26 16:37:48 +0100

    Moved color parsing from ConsoleUI to Menu Helper

--- src/UI/ConsoleUI.cs
+++ src/UI/ConsoleUI.cs
@@ -34,14 +34,7 @@ public class ConsoleUI : MonoBehaviour
             fontSize = 16
         };
 
-        if (ColorUtility.TryParseHtmlString(MalumMenu.menuHtmlColor.Value, out var configUIColor))
-        {
-            GUI.backgroundColor = configUIColor;
-        }
-
         MenuHelper.ApplyMenuColor();
-
-        
 
         _windowRect = GUI.Window(1, _windowRect, (GUI.WindowFunction)ConsoleWindow, "Console");
     }
