3d70e708f755cbf04f2197aed6d1d746e100c628
Frankmon <83294327+Baumdc@users.noreply.github.com>
2026-02-26 16:32:22 +0100

    Add MenuHelper class for menu color application

---
+++ src/UI/MenuHelper.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace MalumMenu;
+
+public static class MenuHelper
+{
+    public static void ApplyMenuColor()
+    {
+        if (CheatToggles.rgbMode)
+        {
+            GUI.backgroundColor = Color.HSVToRGB(MenuUI.hue, 1f, 1f);
+        }
+        else
+        {
+            var configHtmlColor = MalumMenu.menuHtmlColor.Value;
+
+            if (ColorUtility.TryParseHtmlString(configHtmlColor, out var uiColor))
+            {
+                GUI.backgroundColor = uiColor;
+            }
+            else if (!configHtmlColor.StartsWith("#") && ColorUtility.TryParseHtmlString("#" + configHtmlColor, out uiColor))
+            {
+                GUI.backgroundColor = uiColor;
+            }
+        }
+    }
+}
