4712138a081a400851ef49ee12e6ca32cb263fc3
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-08-24 00:59:14 +0200

    chore: set 2026.8.18 as supported AU version

--- src/MalumMenu.cs
+++ src/MalumMenu.cs
@@ -31,7 +31,7 @@ public partial class MalumMenu : BasePlugin
     public static KeybindListener keybindListener;
 
     public static string malumVersion = "3.2.0";
-    public static List<string> supportedAU = new List<string> { "2026.6.5", "2026.3.31" };
+    public static List<string> supportedAU = new List<string> { "2026.8.18" };
     public static bool isPanicked = false;
     public static bool inStealthMode = false;
 
