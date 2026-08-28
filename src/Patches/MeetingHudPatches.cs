d3acc80af485745ab22693e9e71a61b7e49a851c
basugur568-dot <basugur568@gmail.com>
2026-08-28 22:35:35 +0300

    update MeetingHudPatches.js to have a feature where "Reveal Votes" reveals votes in real time
    so we can see who voted who in real time instead of waiting vote to end which can be useful to decide who to vote to tie the vote to save your ass
    
    sorry for my shitty english

--- src/Patches/MeetingHudPatches.cs
+++ src/Patches/MeetingHudPatches.cs
@@ -1,6 +1,8 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace MalumMenu;
@@ -10,7 +12,8 @@ public static class MeetingHud_Update
 {
     public static List<int> votedPlayers = new List<int>();
 
-    // Prefix patch of MeetingHud.Update to constantly bloop new vote icons for each new vote being cast during the meeting
+    private static readonly Dictionary<int, List<GameObject>> _voteLabels = new Dictionary<int, List<GameObject>>();
+
     public static void Prefix(MeetingHud __instance)
     {
         if (__instance.state < MeetingHud.MeetingStates.Results)
@@ -24,22 +27,7 @@ public static class MeetingHud_Update
                 if (playerData != null && !playerData.Disconnected && playerVoteArea.VotedForId != PlayerVoteArea.HasNotVoted && playerVoteArea.VotedForId != PlayerVoteArea.MissedVote && playerVoteArea.VotedForId != PlayerVoteArea.DeadVote && !votedPlayers.Contains(playerVoteArea.PlayerId))
                 {
                     votedPlayers.Add(playerVoteArea.PlayerId);
-
-                    if (playerVoteArea.VotedForId != PlayerVoteArea.SkippedVote)
-                    {
-                        foreach (var votedForArea in __instance.playerStates)
-                        {
-                            if (votedForArea.PlayerId == playerVoteArea.VotedForId)
-                            {
-                                __instance.BloopAVoteIcon(playerData, 0, votedForArea.transform);
-                                break;
-                            }
-                        }
-                    }
-                    else if (__instance.SkippedVoting)
-                    {
-                        __instance.BloopAVoteIcon(playerData, 0, __instance.SkippedVoting.transform);
-                    }
+                    RevealVote(__instance, playerVoteArea, playerData);
                 }
             }
 
@@ -56,7 +44,6 @@ public static class MeetingHud_Update
                 }
             }
 
-            // This is required to see who skipped the voting
             if (__instance.SkippedVoting)
             {
                 __instance.SkippedVoting.SetActive(CheatToggles.revealVotes);
@@ -68,15 +55,87 @@ public static class MeetingHud_Update
     {
         MalumESP.MeetingNametags(__instance);
 
-        // Bugfix: NoClip staying active if meeting is called whilst climbing ladder
         PlayerControl.LocalPlayer.onLadder = false;
+    }
+
+    public static void RevealVote(MeetingHud __instance, PlayerVoteArea area, NetworkedPlayerInfo voter)
+    {
+        if (!CheatToggles.revealVotes) return;
+
+        PlayerVoteArea target = null;
+        GameObject skipTarget = null;
+
+        if (area.VotedForId == PlayerVoteArea.SkippedVote)
+        {
+            skipTarget = __instance.SkippedVoting;
+        }
+        else
+        {
+            foreach (var a in __instance.playerStates)
+            {
+                if (a.PlayerId == area.VotedForId)
+                {
+                    target = a;
+                    break;
+                }
+            }
+        }
+
+        Transform parent = target != null ? target.transform : (skipTarget != null ? skipTarget.transform : null);
+        if (parent == null) return;
+
+        int key = target != null ? (int)target.PlayerId : -1;
+        _voteLabels.TryGetValue(key, out var existing);
+        int stack = existing != null ? existing.Count : 0;
+
+        TextMeshPro template = target != null ? target.NameText : null;
+        if (template == null) template = __instance.playerStates.FirstOrDefault(p => p && p.NameText)?.NameText;
+        if (template == null) return;
+
+        var label = Object.Instantiate(template, parent);
+        label.name = "Reveal_" + voter.PlayerName;
+        label.text = voter.PlayerName;
+        label.color = Palette.PlayerColors[voter.DefaultOutfit.ColorId];
+        label.alignment = TextAlignmentOptions.Center;
+        label.fontSize = 2.0f;
+        label.fontSizeMin = 1.2f;
+        label.fontSizeMax = 2.0f;
+        label.enableWordWrapping = false;
+        label.overflowMode = TextOverflowModes.Overflow;
+        label.outlineWidth = 0.22f;
+        label.outlineColor = Color.black;
+        label.transform.localScale = Vector3.one * 0.95f;
+        label.transform.localPosition = new Vector3(0f, 0.55f + stack * 0.30f, -0.5f);
+        label.gameObject.SetActive(true);
+        label.enabled = true;
+        label.sortingOrder = 20;
+        label.maskable = false;
+
+        if (!_voteLabels.TryGetValue(key, out var list))
+        {
+            list = new List<GameObject>();
+            _voteLabels[key] = list;
+        }
+        list.Add(label.gameObject);
+    }
+
+    public static void ClearVoteLabels()
+    {
+        foreach (var list in _voteLabels.Values)
+        {
+            foreach (var label in list)
+            {
+                if (label) Object.Destroy(label);
+            }
+            list.Clear();
+        }
+        _voteLabels.Clear();
     }
 }
 
 [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.PopulateResults))]
 public static class MeetingHud_PopulateResults
 {
-    // Prefix patch of MeetingHud.PopulateResults to clear all vote icons before repopulating them for final results
     public static void Prefix(MeetingHud __instance)
     {
         foreach (var votedForArea in __instance.playerStates)
@@ -110,16 +169,16 @@ public static class MeetingHud_PopulateResults
         }
 
         MeetingHud_Update.votedPlayers.Clear();
+        MeetingHud_Update.ClearVoteLabels();
     }
 }
 
 [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.CheckForEndVoting))]
 public static class MeetingHud_CheckForEndVoting
 {
-    // Prefix patch of MeetingHud.CheckForEndVoting to make the local player immune to being voted out
     public static bool Prefix(MeetingHud __instance)
     {
-        if (!CheatToggles.voteImmune) return true; // We don't need to check whether we are host because this method only runs on the host's side
+        if (!CheatToggles.voteImmune) return true;
 
         if (!__instance.playerStates.All(ps => ps.AmDead || ps.DidVote)) return true;
 
@@ -147,7 +206,6 @@ public static class MeetingHud_CheckForEndVoting
             }
         }
 
-        // This is the only change from the original method - make sure local player is not exiled
         if (exiled != null && exiled == PlayerControl.LocalPlayer.Data)
         {
             exiled = null;
@@ -168,5 +226,21 @@ public static class MeetingHud_CheckForEndVoting
         __instance.RpcVotingComplete(states, exiled, tie, wasOverruled, overruleNonce);
 
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(PlayerVoteArea), nameof(PlayerVoteArea.Deserialize))]
+public static class PlayerVoteArea_Deserialize
+{
+    public static void Postfix(PlayerVoteArea __instance)
+    {
+        if (MeetingHud.Instance == null || !CheatToggles.revealVotes) return;
+        if (__instance.VotedForId == PlayerVoteArea.HasNotVoted || __instance.VotedForId == PlayerVoteArea.MissedVote || __instance.VotedForId == PlayerVoteArea.DeadVote) return;
+        if (MeetingHud_Update.votedPlayers.Contains((int)__instance.PlayerId)) return;
+        if (!DestroyableSingleton<GameData>.InstanceExists || PlayerControl.LocalPlayer == null) return;
+        var voter = GameData.Instance.GetPlayerById(__instance.PlayerId);
+        if (voter == null || voter.Disconnected) return;
+        MeetingHud_Update.votedPlayers.Add((int)__instance.PlayerId);
+        MeetingHud_Update.RevealVote(MeetingHud.Instance, __instance, voter);
     }
 }
